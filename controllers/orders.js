484d78694b4cc41d57a6f1615d086e6b37b0d0dc
tylersb <tylersb@gmail.com>
2023-01-27 15:08:28 -0800

    temporarily comments out PUT route for /orders/:id until model is refactored to reference

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,17 +1,6 @@
 const router = require('express').Router()
 const db = require('../models')
 
-// GET /orders/:id
-router.get('/:id', async (req, res) => {
-  try {
-    const foundOrder = await db.Order.findById(req.params.id)
-    res.json(foundOrder)
-  } catch (err) {
-    console.log(err)
-    res.status(500).json({ msg: 'Server Error' })
-  }
-})
-
 // POST /orders
 router.post('/', async (req, res) => {
   try {
@@ -31,5 +20,31 @@ router.post('/', async (req, res) => {
     res.status(500).json({ msg: 'Server Error' })
   }
 })
+
+// GET /orders/:id
+router.get('/:id', async (req, res) => {
+  try {
+    const foundOrder = await db.Order.findById(req.params.id)
+    res.json(foundOrder)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ msg: 'Server Error' })
+  }
+})
+
+// PUT /orders/:id
+// router.put('/:id', async (req, res) => {
+//   try {
+//     const updatedOrder = await db.User.Order.findByIdAndUpdate(
+//       req.params.id,
+//       req.body,
+//       { new: true, upsert: true }
+//     )
+//     res.json(updatedOrder)
+//   } catch (err) {
+//     console.log(err)
+//     res.status(500).json({ msg: 'Server Error' })
+//   }
+// })
 
 module.exports = router
