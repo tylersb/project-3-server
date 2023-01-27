00159d8a51a3fe3f21db94183b363b17641a59cb
tylersb <tylersb@gmail.com>
2023-01-27 09:56:35 -0800

    adds phoneNumber & address fields to User model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,38 @@
 // require mongoose ODM
 const mongoose = require('mongoose')
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String
+    },
+    email: {
+      type: String
+    },
+    password: {
+      type: String
+    },
+    phoneNumber: {
+      type: Number
+    },
+    address: {
+      street: {
+        type: String
+      },
+      city: {
+        type: String
+      },
+      state: {
+        type: String
+      },
+      zip: {
+        type: Number
+      }
+    }
   },
-  email: {
-    type: String
-  },
-  password: {
-    type: String
+  {
+    timestamps: true
   }
-}, {
-  timestamps: true
-})
+)
 
 module.exports = mongoose.model('User', UserSchema)
